7a0a1446998e713495738d1018148b0238a440c1
jdhq <jdhqlife@gmail.com>
2023-02-01 23:12:04 -0500

    Setup inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import ResolutionContainer from './components/container/resolutionContainer';
+import ResolutionVisualizer from './components/container/resolutionVisualizer';
+import container_img from "./img/container-background1.jpg"
+import { Resolution } from './models/resolution.class';
 
 function App() {
+
+  const [selectedResolution, setSelectedResolution] = useState<Resolution | null>(null)
+
+  function selectResolution(resolution: Resolution | null) {
+    setSelectedResolution(resolution);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={container_img} alt="" />
+      <ResolutionContainer selectResolution={selectResolution} />
+      {
+          selectedResolution!==null 
+          ? <ResolutionVisualizer resolution = {selectedResolution} selectResolution={selectResolution}/> 
+          : ""
+      }
+      
     </div>
   );
 }
